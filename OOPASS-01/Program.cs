876847bb7cefcb00895a0d4bbd9a2b96e9c421de
Mariam Omara <mariamomara2005@gmail.com>
2026-09-16 00:09:25 +0300

    Q1

--- OOPASS-01/Program.cs
+++ OOPASS-01/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace OOPASS_01
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//namespace OOPASS_01
+//{
+//    //Q1
+//    public struct DeliveryAddress
+//    {
+//        public string City;
+//        public string Street;
+//        public int BuildingNumber;
+
+//        public DeliveryAddress(string city, string street, int buildingNumber)
+//        {
+//            City = city;
+//            Street = street;
+//            BuildingNumber = buildingNumber;
+//        }
+
+//        public string GetFullAddress()
+//        {
+//            return $"{BuildingNumber} {Street}, {City}";
+//        }
+//    }
+
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            DeliveryAddress original =
+//                new DeliveryAddress("Cairo", "Tahrir Street", 15);
+
+//            DeliveryAddress copied = original;
+
+//            copied.BuildingNumber = 20;
+//            copied.Street = "Makram Ebeid Street";
+
+//            Console.WriteLine($"Original Address: {original.GetFullAddress()}");
+//            Console.WriteLine($"Copied Address: {copied.GetFullAddress()}");
+//        }
+//    }
+//}
